2b04c74e0d1cc237e651c10357741cb74f498f3e
IliyanaMStaneva <106416138+IliyanaMStaneva@users.noreply.github.com>
2023-03-23 12:34:03 -0500

    Add files via upload

---
+++ First_notebook.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ## Iliyana Mendoza - desire to be a data scientist
+
+# *I am interested in data science because data-driven decisions produce better results in the long run*
+
+# ### The code in the cell four is suming the digits 1+1
+
+# In[1]:
+
+
+x = 1 
+y = 1
+print(x+y)
+
+
+# ![221218184732-messi-wc-trophy.jpeg](attachment:221218184732-messi-wc-trophy.jpeg)
+
+# 1. Data
+# 2. Science
+# 3. Is
+# 4. Useful
+
+# Data science is useful
+# ***
+# Lionel Messi is better
+# 
+
+# In[ ]:
+
+
+
+
